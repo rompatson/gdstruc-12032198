ccba64534ceb13c292fecb90a6068ccd4bf08df0
Romar Patrick Sonico <romarpatrick.sonico@benilde.edu.ph>
2021-09-01 12:35:56 +0800

    Answer for Quiz 1

---
+++ Quiz 1/src/com/romarpatricksonico/Main.java
@@ -0,0 +1,74 @@
+package com.romarpatricksonico;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+
+        numbers[0] = 35;
+        numbers[1] = 69;
+        numbers[2] = 1;
+        numbers[3] = 10;
+        numbers[4] = -50;
+        numbers[5] = 320;
+        numbers[6] = 63;
+        numbers[7] = 58;
+        numbers[8] = 26;
+        numbers[9] = 13;
+
+        System.out.println("Before Sort");
+        printArrayElements(numbers);
+
+        System.out.println("\n\n Bubble Sort (Descending Order)");
+        bubbleSort(numbers);
+        printArrayElements(numbers);
+
+        System.out.println("\n\n Selection Sort (Descending Order)");
+        selectionSort(numbers);
+        printArrayElements(numbers);
+    }
+
+    private static void bubbleSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            for (int i = 0; i < lastSortedIndex; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void selectionSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            int largestIndex = 0;
+
+            for(int i = 1; i <= lastSortedIndex; i++)
+            {
+                if (arr[i] < arr[largestIndex])
+                {
+                    largestIndex = i;
+                }
+            }
+
+            int temp = arr[lastSortedIndex];
+            arr[lastSortedIndex] = arr[largestIndex];
+            arr[largestIndex] = temp;
+        }
+    }
+
+    private static void printArrayElements(int[] arr)
+    {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
